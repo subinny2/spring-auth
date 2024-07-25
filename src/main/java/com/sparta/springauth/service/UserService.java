8758ac2a5720808d8ab22e628710fd9a2a11a551
subinny2 <116608047+subinny2@users.noreply.github.com>
2024-07-25 11:37:12 +0900

    접근 불가 페이지

--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -56,26 +56,4 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
-
-        // 사용자 확인
-        User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
-        );
-        
-        // 비밀번호 확인
-        if(!passwordEncoder.matches(password, user.getPassword())) {
-                                //  평문비밀번호, 암호화된 비밀번호
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-        
-        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
-        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
-                                        //인증이 완료된 username, 사용자의 권한
-        jwtUtil.addJwtToCookie(token, res);
-
-
-    }
 }
