661db6f7d777f170b13b6344d68d3f47470dca5b
subinny2 <116608047+subinny2@users.noreply.github.com>
2024-07-25 10:44:10 +0900

    Spring Security 로그인

--- src/main/java/com/sparta/springauth/filter/AuthFilter.java
+++ src/main/java/com/sparta/springauth/filter/AuthFilter.java
@@ -14,7 +14,7 @@ import org.springframework.util.StringUtils;
 import java.io.IOException;
 
 @Slf4j(topic = "AuthFilter")
-@Component
+//@Component
 @Order(2) // doFilter가 여기를 탄다.
 public class AuthFilter implements Filter {
     
